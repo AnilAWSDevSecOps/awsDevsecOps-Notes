ed243de48c4c1468d98d987b72526218047681e5
AnilAWSDevSecOps <anilawsdevsecops@gmail.com>
2025-07-06 10:20:23 +0000

    -Sun Jul  6 10:20:23 AM UTC 2025

--- Shell_Scripting/Shell_Scripts/11-MemUtilSlackNotify.sh
+++ Shell_Scripting/Shell_Scripts/11-MemUtilSlackNotify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script to find the memory utilization of the system
-WebHook='https://hooks.slack.com/services/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'
+WebHook='https://hooks.slack.com/services/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'
 HostName=$(curl -sL http://169.254.169.254/latest/meta-data/local-hostname)
 Total_Mem=$(free -m | grep Mem | awk '{print $2}')
 Available_Mem=$(free -m | grep Mem | awk '{print $7}')
@@ -10,7 +10,9 @@ echo "Total Memory of $HostName: $Total_Mem MB"
 echo "Available Memory of $HostName: $Available_Mem MB"
 echo "Used Memory of $HostName: $Used_Mem MB"
 echo "Memory Utilization of $HostName: $Mem_Utilization %"
-if [ $(echo "$Mem_Utilization > 90" | bc) -eq 1 ]; then
+# Uncomment below line only for testing since memory cant be high.
+if [ $(echo "$Mem_Utilization < 90" | bc) -eq 1 ]; then
+# if [ $(echo "$Mem_Utilization > 90" | bc) -eq 1 ]; then
     echo "Memory utilization is high of $HostName"
     curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"Memory utilization of $HostName is high. Memory Utilization of $HostName: $Mem_Utilization %\"}" $WebHook
     curl -X POST $WebHook -sL -H 'Content-type: application/json' --data "{\"text\":\"Memory utilization of $HostName is high. Memory Utilization of $HostName: $Mem_Utilization %\"}" >>/dev/null
